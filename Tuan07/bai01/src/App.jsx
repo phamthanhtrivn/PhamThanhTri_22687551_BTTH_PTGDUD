11b46c2b525a1a5a9981d1a6cfda4e9151c5ed3b
Tri Pham <phamthanhtri0712@gmail.com>
2025-04-09 16:40:01 +0700

    5 - 6 - 7. isActive-NavLink, React Router, SPA

--- Tuan07/bai01/src/App.jsx
+++ Tuan07/bai01/src/App.jsx
@@ -1,7 +1,12 @@
-import Header from "./components/Header"
-import SideBarMenu from "./components/SideBarMenu"
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import SideBarMenu from "./components/SideBarMenu";
 import DashBoard from "./pages/DashBoard"
-
+import Teams from "./pages/Teams";
+import Projects from "./pages/Projects";
+import Analytics from "./pages/Analytics";
+import Messages from "./pages/Messages";
+import Integrations from "./pages/Integrations";
 
 const App = () => {
   return (
@@ -11,11 +16,17 @@ const App = () => {
       </div>
       <div>
         <Header />
-        <DashBoard />
+        <Routes>
+          <Route path="/" element={<DashBoard />} />
+          <Route path="/teams" element={<Teams />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/integrations" element={<Integrations />} />
+        </Routes>
       </div>
     </div>
-
   )
 }
 
-export default App
+export default App;
